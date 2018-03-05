4720504acb450bd6d1395fd20b7b556a33380af2
Danielle Scott <danielle.scott02@mymona.uwi.edu>
2018-03-05 02:50:19 +0000

    Step 8

--- app/views.py
+++ app/views.py
@@ -34,7 +34,7 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
@@ -43,10 +43,18 @@ def login():
             # passed to the login_user() method.
 
             # get user id, load into session
+            
+            username = form.username.data
+            password = form.password.data
+            
+            user = UserProfile.query.filter_by(username=username).first()
+            pword = UserProfile.query.filter_by(password=password).first()
+            
             login_user(user)
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash('Log in successful.', 'success')
+            return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
